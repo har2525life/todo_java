d892eb86ef290493e6017fb3490ac4431ad66d8d
har2525life <har2525life@outlook.com>
2023-08-02 10:06:28 +0900

    add: axios package, fetch todo

--- todo_react/src/App.tsx
+++ todo_react/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    axios.get("localhost:8081").then((response) => {
+      console.log(response)
+    })
+  }, [])
 
   return (
     <>
