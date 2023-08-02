e72404f7b485d7d0ac09051a7e770394d0b0b4d1
har2525life <har2525life@outlook.com>
2023-08-02 12:04:56 +0900

    add backend: get, post method

--- todo_java/src/main/java/com/example/todo_java/controller/TodoApplicationController.java
+++ todo_java/src/main/java/com/example/todo_java/controller/TodoApplicationController.java
@@ -1,15 +1,36 @@
 package com.example.todo_java.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class TodoApplicationController {
     
     @GetMapping("/")
-    public String fetchTodo() {
-        return "hello world";
+    public List fetchTodo() {
+        List<Map<String, String>> todos = new ArrayList<>();
+
+        Map<String, String> todo1 = new HashMap<>();
+        todo1.put("id1", "task1");
+        todos.add(todo1);
+        return todos;
     }
+
+    @PostMapping("/add")
+    public Map addTodo() {
+        Map<String, String> todo = new HashMap<>();
+
+        todo.put("id10", "task10");
+
+        return todo;
+    }
+
 }
